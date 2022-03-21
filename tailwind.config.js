ab3b1b8f5cab83bcf80d05872c4b46bfc254c69f
Yuan <crecpz@gmail.com>
2022-03-21 18:11:28 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    'index.html', 'main.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#BCB8B8',
+      },
+      fontFamily: {
+        'space-mono': "'Space Mono', monospace",
+      }
+    },
+  },
+  plugins: [],
+}
