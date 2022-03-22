31309b6d60a5c400988ea01e03efa121fca1be79
Yuan <crecpz@gmail.com>
2022-03-22 23:45:22 +0800

    目前已經將input 和 btn重疊了，接下來要決定他們的層級關係

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector('#btn')
-
+let searchBar = document.querySelector('#search-bar')
 
 btn.addEventListener('click',()=>{
     btn.classList.toggle('min-w-full')
+    searchBar.classList.toggle('opacity-0')
 })
