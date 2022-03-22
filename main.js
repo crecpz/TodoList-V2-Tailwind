b1c5524d23680b5f64c872f58a53a51d930694dc
Yuan <crecpz@gmail.com>
2022-03-22 18:28:36 +0800

    成功做出一個跟button長得一樣的input 目前內容物:button、input (然後他們長一樣)

---
+++ main.js
@@ -0,0 +1,7 @@
+// let btn = document.querySelector('#btn')
+// btn.style.width = btn.clientWidth;
+// btn.style.transition = 'width 1000ms';
+// btn.addEventListener('click',()=>{
+//     btn.style.width = '100%'
+// })
+// console.log(btn.clientWidth);
