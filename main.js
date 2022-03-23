40fb91dc05476facff5a3d953285ce58c66f2961
Yuan <crecpz@gmail.com>
2022-03-23 18:15:49 +0800

    1.search部分的input與btn已經可以重疊，並且點擊後可以成功將搜尋input置於btn之上，此時btn是隱藏狀態。 2.可以點擊搜尋框內的打叉按鈕，第一次按:刪除文字，第二次按:關閉搜尋框。

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 let btn = document.querySelector('#btn')
 let searchBar = document.querySelector('#search-bar')
+btn.addEventListener('click', () => {
+    btn.classList.add('opacity-0')
+    btn.classList.add('invisible')
 
-btn.addEventListener('click',()=>{
-    btn.classList.toggle('min-w-full')
+    searchBar.classList.remove('invisible')
+    searchBar.classList.add('visible')
     searchBar.classList.toggle('opacity-0')
+    searchBar.classList.toggle('-z-[1]')
+})
+
+// close search btn
+let closeSearchBtn = document.querySelector('#search-close-btn')
+let searchInput = document.querySelector('#search-input')
+
+
+closeSearchBtn.addEventListener('click', () => {
+    if (searchInput.value === '') {
+        btn.classList.remove('opacity-0')
+        btn.classList.remove('invisible')
+        searchBar.classList.add('invisible')
+        searchBar.classList.remove('visible')
+        searchBar.classList.toggle('opacity-0')
+        searchBar.classList.toggle('-z-[1]')
+    }else{
+        searchInput.value = '';
+    }
 })
