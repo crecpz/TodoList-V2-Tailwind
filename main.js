50ab0485f34f9dff7daef3d156c980a3f22b0adc
Yuan <crecpz@gmail.com>
2022-03-28 20:14:14 +0800

    1.btn的hamberger動畫製作中 2.更改了h1與hamberger的擺放方式(hamberger原本在右邊->現在在左邊) 3.修正輸入框與按鈕的布局方式(部分物件的absolute被我刪去，改以正常流)

--- main.js
+++ main.js
@@ -2,14 +2,12 @@
 let menuBtn = document.querySelector('#menu-btn')
 let menu = document.querySelector('#menu')
 let title = document.querySelector('#title')
-menuBtn.addEventListener('click', ()=>{
-    menu.classList.toggle('hide')
-    title.classList.toggle('hide')
-    // if(menu.classList.contains('hide')){
-    //     title.classList.add('hide')
-    // }else{
-
-    // }
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('item-center')
+    menu.classList.toggle('justify-end')
+    title.classList.toggle('hidden')
 })
 
 
@@ -18,15 +16,19 @@ let searchWrapper = document.querySelector('#search-wrapper')
 let completedBtn = document.querySelector('#completed-btn')
 
 searchBtn.addEventListener('click', () => {
-    searchBtn.classList.add('opacity-0')
-    searchBtn.classList.add('invisible')
-    completedBtn.classList.add('opacity-0')
-    completedBtn.classList.add('invisible')
+    searchBtn.classList.add('hidden')
+    completedBtn.classList.add('hidden')
+    searchWrapper.classList.remove('hidden')
 
-    searchWrapper.classList.remove('invisible')
-    searchWrapper.classList.add('visible')
-    searchWrapper.classList.toggle('opacity-0')
-    searchWrapper.classList.toggle('-z-[1]')
+    // searchBtn.classList.add('opacity-0')
+    // searchBtn.classList.add('invisible')
+    // completedBtn.classList.add('opacity-0')
+    // completedBtn.classList.add('invisible')
+
+    // searchWrapper.classList.remove('invisible')
+    // searchWrapper.classList.add('visible')
+    // searchWrapper.classList.toggle('opacity-0')
+    // searchWrapper.classList.toggle('-z-[1]')
 })
 
 // close search btn
@@ -35,17 +37,18 @@ let searchInput = document.querySelector('#search-input')
 
 
 closeSearchBtn.addEventListener('click', () => {
-    if (searchInput.value === '') {
-        searchBtn.classList.remove('opacity-0')
-        searchBtn.classList.remove('invisible')
-        completedBtn.classList.remove('opacity-0')
-        completedBtn.classList.remove('invisible')
 
-        searchWrapper.classList.add('invisible')
-        searchWrapper.classList.remove('visible')
-        searchWrapper.classList.toggle('opacity-0')
-        searchWrapper.classList.toggle('-z-[1]')
-    } else {
-        searchInput.value = '';
-    }
+    // if (searchInput.value === '') {
+    //     searchBtn.classList.remove('opacity-0')
+    //     searchBtn.classList.remove('invisible')
+    //     completedBtn.classList.remove('opacity-0')
+    //     completedBtn.classList.remove('invisible')
+
+    //     searchWrapper.classList.add('invisible')
+    //     searchWrapper.classList.remove('visible')
+    //     searchWrapper.classList.toggle('opacity-0')
+    //     searchWrapper.classList.toggle('-z-[1]')
+    // } else {
+    //     searchInput.value = '';
+    // }
 })
