84e4989fe0904496e4c4cc371c2f381e4cd629e6
Yuan <crecpz@gmail.com>
2022-03-29 20:13:03 +0800

    1.漢堡條動畫製作中(研究怎麼寫進tailwind.config.js) 2.布局細節修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,37 @@ module.exports = {
       },
       fontFamily: {
         'space-mono': "'Space Mono', monospace",
-      }
+      },
+      keyframes: {
+        'before-expand': {
+          '50%': {
+            top:'50%',
+            'translate-y':'-50%'
+          },
+          '100%':{
+            top:'50%',
+            transform:'translate-y(-50%)',
+            transform:'rotate(45deg)'
+          },
+        },
+
+        'after-expand': {
+
+        },
+
+        'disappear': {
+
+        },
+      },
+      animation: {
+        'before-expand': 'ease-in-out 1000ms forwards',
+        'after-expand': 'ease-in-out 1000ms forwards',
+        'disappear': 'ease-in-out 500ms forwards',
+      },
     },
-    container:{
-      center:true,
-      padding:'1rem',
+    container: {
+      center: true,
+      padding: '1rem',
     }
   },
   plugins: [],
