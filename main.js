52e0f6142e4c4b8efd3beb8fd29c354cd654236a
Yuan <crecpz@gmail.com>
2022-04-01 14:06:31 +0800

    將js的各種程式碼提取出來做命名，使其可以用一個名稱來表示這個function在做什麼。另外，也將一些程式做一些整理。

--- main.js
+++ main.js
@@ -1,36 +1,61 @@
-// hamberger menu
+// hamburger menu
 let menuBtn = document.querySelector('#menu-btn')
 let menu = document.querySelector('#menu')
 let title = document.querySelector('#title')
 let hamburger = document.querySelector('#hamburger')
 
+// Homepage <---> menu
+const backToHomepage = () => {
+    menu.classList.add('hidden');
+    menu.classList.remove('flex')
+    menu.classList.remove('item-center')
+    menu.classList.remove('justify-end')
+    title.classList.remove('hidden')
+    menuBtn.removeEventListener('click', backToHomepage)
+    menuBtn.addEventListener('click', showMenu)
+}
+const showMenu = () => {
+    menu.classList.remove('hidden');
+    menu.classList.add('flex')
+    menu.classList.add('item-center')
+    menu.classList.add('justify-end')
+    title.classList.add('hidden')
+    menuBtn.removeEventListener('click', showMenu)
+    menuBtn.addEventListener('click', backToHomepage)
+}
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('hidden')
-    menu.classList.toggle('flex')
-    menu.classList.toggle('item-center')
-    menu.classList.toggle('justify-end')
-    title.classList.toggle('hidden')
-    
-    if(!hamburger.classList.contains('hamburger-expand')){
+// hamburger switcher
+const hamburgerSwitcher = () => {
+    if (!hamburger.classList.contains('hamburger-expand')) {
         hamburger.classList.remove('hamburger-collapse')
         hamburger.classList.add('hamburger-expand')
-    }else{
+    } else {
         hamburger.classList.remove('hamburger-expand')
         hamburger.classList.add('hamburger-collapse')
     }
-})
+}
 
+// open search input
+const openSearchInput = () => {
+    searchBtn.classList.add('hidden')
+    completedBtn.classList.add('hidden')
+    searchWrapper.classList.remove('hidden')
+
+    menuBtn.removeEventListener('click', showMenu)
+    menuBtn.removeEventListener('click', backToHomepage)
+    menuBtn.removeEventListener('click', hamburgerSwitcher)
+    // ↓ 這行有問題，因為showMenu自帶刪除己身之事件監聽，所以不能這樣用。
+    menuBtn.addEventListener('click', showMenu)
+}
+
+menuBtn.addEventListener('click', hamburgerSwitcher)
+menuBtn.addEventListener('click', showMenu)
 
 let searchBtn = document.querySelector('#search-btn')
 let searchWrapper = document.querySelector('#search-wrapper')
 let completedBtn = document.querySelector('#completed-btn')
 
-searchBtn.addEventListener('click', () => {
-    searchBtn.classList.add('hidden')
-    completedBtn.classList.add('hidden')
-    searchWrapper.classList.remove('hidden')
-})
+searchBtn.addEventListener('click', openSearchInput)
 
 // close search btn
 let closeSearchBtn = document.querySelector('#search-close-btn')
