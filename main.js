197a45f3059e1f6a8ceb8a761e01241639e65bba
Yuan <crecpz@gmail.com>
2022-04-19 19:57:54 +0800

    1.目前已經可以做增加、刪除的操作了，而且f5也不會消失的那種!

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ menuBtn.addEventListener('click', fadeOutTitle)
 menuBtn.addEventListener('click', fadeInMenu)
 
 
-/* Todo Input */
+/* Todo Input - click addBtn or press Enter to add new todo */
 const todoInput = document.querySelector('#todo-input')
 const addBtn = document.querySelector('#add-btn')
 todoInput.addEventListener('keyup', e => {
@@ -30,7 +30,6 @@ todoInput.addEventListener('keyup', e => {
     }
 })
 addBtn.addEventListener('click', addNewTodo)
-
 
 
 /* Todo List */
@@ -44,9 +43,9 @@ renderTodo()
 function renderTodo() {
     let todoItems = '';
     if (todoListData) {
-        todoListData.forEach((data) => {
+        todoListData.forEach((data, index) => {
             todoItems +=
-                `<li class="todo-item">
+                `<li class="todo-item" id="${index}">
                     <label class="flex items-center justify-between w-full">
                         <input type="checkbox" class="todo-checkbox">
                         <p class="todo-text">${data.content}</p>
@@ -63,30 +62,13 @@ function renderTodo() {
                                 class="fa-solid fa-trash ml-2"></i></button>
                     </div>
                 </li>`
-                // `<li class="todo-item">
-                //     <div class="flex items-center justify-between w-full">
-                //         <input type="checkbox" class="todo-checkbox">
-                //         <p class="todo-text">${data.content}</p>
-                //         <button class="todo-option-btn">
-                //             <div class="todo-option-btn__dot"></div>
-                //             <div class="todo-option-btn__dot"></div>
-                //             <div class="todo-option-btn__dot"></div>
-                //         </button>
-                //     </div>
-                //     <div class="todo-option">
-                //         <button class="edit-btn btn btn--small btn--black mr-4">Edit<i
-                //                 class="fa-solid fa-pen-to-square ml-2"></i></button>
-                //         <button class="remove-btn btn btn--small btn--black">Remove<i
-                //                 class="fa-solid fa-trash ml-2"></i></button>
-                //     </div>
-                // </li>`
             todoList.innerHTML = todoItems;
         })
     }
-
 }
 
 function addNewTodo() {
+
     if (todoInput.value.trim() !== '') {
         if (!todoListData) {
             todoListData = [];
@@ -108,35 +90,62 @@ function addNewTodo() {
     }
 }
 
+// if (todoList) {
+// }
+
+// 0419 目前有個小問題是在於當我的todo空無一物的時候，我點todolist會有報錯，
+// 因為我監聽器裡面所聲明的那些東西根本都還不存在。
 
 todoList.addEventListener('click', e => {
+
     const todoItem = e.target.closest('li')
-    const label = todoItem.querySelector('label')
+    // const label = todoItem.querySelector('label')
+    // const editBtn = todoItem.querySelector('.edit-btn')
 
-
-    // 如果用戶點按todo-option-btn，使其展開todo-option
+    /* 如果用戶點按todo-option-btn，使其展開todo-option */
     const todoOption = todoItem.querySelector('.todo-option');
     if (e.target.classList.value === 'todo-option-btn') {
         todoOption.classList.toggle('todo-option--open')
     }
 
-    // ★處改成寫進來
+    // 如果用戶點按的class是remove-btn，刪除該項
+    if (e.target.classList.contains('remove-btn')) {
+        removeTodo()
+        if (todoListData.length === 0) {
+            todoList.innerHTML = '';
+        }
+    }
 
+    function removeTodo() {
+        todoListData = todoListData.filter(data => data !== todoListData[todoItem.id])
+        console.log(todoListData);
+        localStorage.setItem('todos', JSON.stringify(todoListData))
+        renderTodo()
+    }
 
 })
+
+
+
+
 
 
 
 // ★
 // 使editBtn按下去之後該文字區域變成可編輯狀態。
-const editBtn = document.querySelectorAll('.edit-btn')
-editBtn.forEach(btn => {
-    btn.addEventListener('click', e => {
-        const todoText = e.target.parentElement.parentElement.querySelector('.todo-text')
-        todoText.setAttribute('contenteditable', 'true')
-    })
-})
-//
+// 有問題!這邊只取到靜態的元素，你只能靠重新整理讓這按鈕能動
+
+
+// const editBtn = document.querySelectorAll('.edit-btn')
+// editBtn.forEach(btn => {
+//     btn.addEventListener('click', e => {
+//         const todoText = e.target.parentElement.parentElement.querySelector('.todo-text')
+//         console.log(todoText)
+
+//         todoText.setAttribute('contenteditable', 'true')
+//     })
+// })
+
 
 
 // 以下兩行是受到selectText函數啟發，原來這樣寫也行，我的作法多套了一層function。但這兩還最後還是會出現閃爍
@@ -155,7 +164,6 @@ function selectText(e, node) {
     label.classList.add('pointer-events-none')
 
     node = todoItem.querySelector('.todo-text');
-
     node.style.border = '1px dashed'
     if (document.body.createTextRange) {
         const range = document.body.createTextRange();
@@ -171,6 +179,8 @@ function selectText(e, node) {
         console.warn("Could not select text in node: Unsupported browser.");
     }
 }
+
+
 const clickable = document.querySelectorAll('.edit-btn');
 clickable.forEach(item => {
     item.addEventListener('click', e => selectText(e, 'todo-text'));
