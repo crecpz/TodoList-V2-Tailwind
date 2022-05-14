f33436c6cba1a952cf92eff4ccc28dc3872de6ca
Yuan <crecpz@gmail.com>
2022-05-14 23:19:49 +0800

    0514

--- main.js
+++ main.js
@@ -155,14 +155,14 @@ function renderTodo(currentTab) {
                             </button>
                         </label>
                         <div class="todo-option">
-                            <button class="edit-btn btn  mr-6"><i
+                            <button class="edit-btn btn mr-4"><i
                                     class="fa-solid fa-pen-to-square mr-2"></i>編輯</button>
                             <button class="remove-btn btn btn-hightlight"><i
                                     class="fa-solid fa-trash mr-2"></i>刪除</button>
                         </div>
                     </li>`
             }
-            todoList.innerHTML = todoItems || '<p id="empty-msg" class="text-primary-darken text-center py-4">Is empty here.</p>';
+            todoList.innerHTML = todoItems || '<p id="empty-msg" class="text-primary text-center py-4">Is empty here.</p>';
         })
     }
 }
@@ -185,10 +185,8 @@ function addedPrompt() {
 
 
 function checkIfListEmpty() {
-    // document.querySelector('#todo-list')
-    // console.log(todoList.children)
     if (todoListData.length === 0) {
-        todoList.innerHTML = '<p id="empty-msg" class="text-primary-darken text-center py-4">Is empty here.</p>';
+        todoList.innerHTML = '<p id="empty-msg" class="text-primary text-center py-4">Is empty here.</p>';
     }
 }
 
@@ -402,7 +400,6 @@ todoList.addEventListener('click', e => {
                     dialog.addEventListener('animationend', () => {
                         dialog.close();
                         dialog.removeAttribute('closing', '')
-                        // dialog.removeAttribute('open', '')
                     }, { once: true })
                 }
 
@@ -442,154 +439,6 @@ function clearCompleted() {
         return data.status !== 'completed';
     })
     renderTodo(currentTab)
+    checkIfListEmpty()
     localStorage.setItem('todos', JSON.stringify(todoListData))
-}
-
-
-
-function fadeOutTitle() {
-    fadeOutItem(title, showMenu, menuBtn, fadeOutTitle, fadeInTitle)
-}
-
-function fadeInMenu() {
-    fadeInItem(menu, showMenu, menuBtn, fadeInMenu, fadeOutMenu)
-}
-
-function fadeOutMenu() {
-    fadeOutItem(menu, backToHomepage, menuBtn, fadeOutMenu, fadeInMenu)
-}
-
-function fadeInTitle() {
-    fadeInItem(title, backToHomepage, menuBtn, fadeInTitle, fadeOutTitle)
-}
-
-
-// (要fadeOut的對象, 結束動畫後要執行的函數, 事件觸發對象, 事件觸發對象callback函數) 
-function fadeInItem(targetItem, callback, evenTarget, removeCallback, addCallback) {
-    targetItem.classList.add('animation-fade-in')
-    targetItem.addEventListener('animationend', () => {
-        menuBtn.classList.remove('pointer-events-none')
-        evenTarget.removeEventListener('click', removeCallback)
-        callback()
-        targetItem.classList.remove('animation-fade-in')
-        evenTarget.addEventListener('click', addCallback)
-        // menuBtn.disabled = false;
-    })
-}
-
-function fadeOutItem(targetItem, callback, evenTarget, removeCallback, addCallback) {
-    targetItem.classList.add('animation-fade-out')
-    targetItem.addEventListener('animationend', () => {
-        menuBtn.classList.remove('pointer-events-none')
-        callback()
-        targetItem.classList.remove('animation-fade-out')
-        evenTarget.removeEventListener('click', removeCallback)
-        evenTarget.addEventListener('click', addCallback)
-    })
-}
-
-
-
-
-// hamburger switcher(expand & collapse)
-function hamburger_expandSwitcher() {
-    menuBtn.disabled = true;
-    let disabledTime = setTimeout(() => {
-        menuBtn.disabled = false;
-        // clearTimeout(disabledTime)
-        // console.log('clear!');
-    }, 600)
-
-
-
-    // menuBtn.classList.add('pointer-events-none')
-    if (!hamburger.classList.contains('hamburger-expand')) {
-        hamburger.classList.remove('hamburger-collapse')
-        hamburger.classList.add('hamburger-expand')
-    } else {
-        hamburger.classList.remove('hamburger-expand')
-        hamburger.classList.add('hamburger-collapse')
-    }
-}
-
-function hamburger_arrowSwitcher() {
-    if (hamburger.classList.contains('hamburger-expand')) {
-        hamburger.classList.add('hamburger-arrow')
-        hamburger.classList.remove('hamburger-expand')
-    } else {
-        hamburger.classList.add('hamburger-expand')
-        hamburger.classList.remove('hamburger-arrow')
-    }
-}
-
-function showMenu() {
-    style_hideTitle();
-    style_showMenu();
-    menuBtn.removeEventListener('click', showMenu);
-}
-
-function backToHomepage() {
-    style_hideMenu()
-    style_showTitle()
-    menuBtn.removeEventListener('click', backToHomepage)
-}
-
-function openSearchInput() {
-    style_showSearchInput()
-    style_hideMenu()
-
-    // 移除無用監聽器:
-    menuBtn.removeEventListener('click', backToHomepage)
-    menuBtn.removeEventListener('click', hamburger_expandSwitcher)
-
-    // 樣式調整:
-    menuBtn.addEventListener('click', hamburger_arrowSwitcher)
-
-    // 新增所需監聽器:
-    menuBtn.addEventListener('click', closeSearchInput);
-}
-
-function closeSearchInput() {
-    style_hideSearchInput()
-    showMenu()
-    menuBtn.removeEventListener('click', closeSearchInput);
-    menuBtn.removeEventListener('click', hamburger_arrowSwitcher);
-    menuBtn.addEventListener('click', backToHomepage);
-    menuBtn.addEventListener('click', hamburger_expandSwitcher);
-}
-
-/* style控制系列 */
-
-function style_hideTitle() {
-    title.classList.add('hidden')
-}
-
-function style_showTitle() {
-    title.classList.remove('hidden')
-}
-
-function style_showMenu() {
-    menu.classList.remove('hidden')
-    menu.classList.add('flex')
-    menu.classList.add('item-center')
-    menu.classList.add('justify-end')
-}
-
-function style_hideMenu() {
-    menu.classList.add('hidden')
-    menu.classList.remove('flex')
-    menu.classList.remove('item-center')
-    menu.classList.remove('justify-end')
-}
-
-function style_hideSearchInput() {
-    searchBtn.classList.remove('hidden')
-    clearBtn.classList.remove('hidden')
-    searchWrapper.classList.add('hidden')
-}
-
-function style_showSearchInput() {
-    searchBtn.classList.add('hidden')
-    clearBtn.classList.add('hidden')
-    searchWrapper.classList.remove('hidden')
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,11 @@ module.exports = {
         primary:'#405362',
         secondary:'#eee5dc',
         tertiary:'#efc881',
-        
-        // primary: '#464543',
-        // 'primary-light':'#9b9a98',
-        // 'primary-dark':'#333332',
-        // secondary:'#dbc3ab',
       },
 
       fontFamily: {
         'Source-Code-Pro': "'Source Code Pro', monospace,'Noto Sans TC','Noto Serif TC'",
+        'Font-Awesome':"'Font Awesome 5 Free'",
       },
     },
   },
