0512d9bca599dc29b3a7adb8ea70c8fd83db0a18
Yuan <crecpz@gmail.com>
2022-07-13 15:33:16 +0800

    0713

--- main.js
+++ main.js
@@ -746,3 +746,8 @@ function clearCompleted() {
         return data.status !== 'completed';
     })
 }
+
+
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
